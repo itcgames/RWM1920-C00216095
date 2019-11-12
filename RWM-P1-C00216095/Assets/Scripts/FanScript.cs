2b7e59102b5c95a331be5f932f331669c6e194ef
Alex OToole <tooler1999@gmail.com>
2019-11-12 12:18:42 +0000

    Finished feature 1

--- RWM-P1-C00216095/Assets/Scripts/FanScript.cs
+++ RWM-P1-C00216095/Assets/Scripts/FanScript.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 
 public class FanScript : MonoBehaviour
 {
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         Destroy(other.gameObject);
     }
+
 }
