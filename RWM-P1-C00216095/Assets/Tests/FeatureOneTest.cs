2b944e365e8d9370d62fb4039015c44f90740bad
Alex OToole <tooler1999@gmail.com>
2019-11-28 11:56:16 +0000

    added particles to make it look like a rocket is making the fan float

---
+++ RWM-P1-C00216095/Assets/Tests/FeatureOneTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FeatureOneTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FeatureOneTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator FeatureOneTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
