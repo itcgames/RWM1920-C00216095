fe071a0823377338569a4a8970c018ccf39c269c
Alex OToole <tooler1999@gmail.com>
2020-01-20 09:21:59 +0000

    latest version

--- RWM-P1-C00216095/Assets/Fan/Scripts/FanScript.cs
+++ RWM-P1-C00216095/Assets/Fan/Scripts/FanScript.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class FanScript : MonoBehaviour
 {
+    public GameObject m_obj;
+    public GameObject Explosion;
+    float criticalVelocity = 9;
+    int damageCount = 3;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Destroy(other.gameObject);
+        Debug.Log(m_obj.GetComponent<Rigidbody2D>().velocity.y * -1);
+        if ((m_obj.GetComponent<Rigidbody2D>().velocity.y * -1) > criticalVelocity)
+        {
+            damageCount--;
+            Debug.Log(damageCount);
+            if (damageCount == 0)
+            {
+                Instantiate(Explosion, gameObject.transform.position, gameObject.transform.rotation);
+                Destroy(gameObject);
+            }
+        }
     }
 
 }
