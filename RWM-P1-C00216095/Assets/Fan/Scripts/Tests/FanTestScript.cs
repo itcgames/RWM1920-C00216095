60a0233ac676f62be266fcec0e2053b6b23f0f42
Alex OToole <tooler1999@gmail.com>
2020-01-23 15:06:48 +0000

    added tests and edited colliders

---
+++ RWM-P1-C00216095/Assets/Fan/Scripts/Tests/FanTestScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class FanTestScript
+    {
+        GameObject Fan = Resources.Load("Fan") as GameObject;
+        GameObject TestBall = Resources.Load("TestBall") as GameObject;
+        //Tests that the Fan is in the scene
+        [UnityTest]
+        public IEnumerator SearchTest()
+        {
+            Assert.IsNotNull(Fan);
+            yield return null;
+        }
+
+        //Tests to see if the fan is repelled upwards
+        [UnityTest]
+        public IEnumerator VelocityTest()
+        {
+            bool velocityFlipped = false;
+            if (TestBall.GetComponent<Rigidbody2D>().velocity.y > 1)
+            {
+                velocityFlipped = true;
+            }
+            yield return velocityFlipped;
+        }
+    }
+}
